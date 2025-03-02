01624cb5c2b76c923847cb3b3bac0279ae7dd388
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-03-02 10:45:53 +0100

    Update run_undi.bat

--- run_undi.bat
+++ run_undi.bat
@@ -1,4 +1,4 @@
 cd /d "%~dp0"
-python abliterate.py -m E:\text-generation-webui\models\cognitivecomputations_Dolphin3.0-Llama3.2-1B -o E:\text-generation-webui\models\cognitivecomputations_Dolphin3.0-Llama3.2-1B_abliterated_LPL --i E:\intermed\ --scan-all --device cuda:0 --flash-attn --precision bf16
+python abliterate.py -m E:\text-generation-webui\models\Nexesenex_Llama_3.2_3b_Costume_0.1 -o E:\text-generation-webui\models\Nexesenex_Llama_3.2_3b_Costume_0.1_abliterated_LPL --i E:\intermed\ --scan-all --device cuda:0 --flash-attn --precision bf16
 pause
 pause
