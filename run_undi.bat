d5628c25af14fdfa8530bf79cc7ea4b528435a01
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-03-06 16:12:54 +0100

    Update run_undi.bat

--- run_undi.bat
+++ run_undi.bat
@@ -1,4 +1,44 @@
-cd /d "%~dp0"
-python abliterate.py -m E:\text-generation-webui\models\Nexesenex_Llama_3.2_3b_Costume_0.1 -o E:\text-generation-webui\models\Nexesenex_Llama_3.2_3b_Costume_0.1_abliterated_LPL --i E:\intermed\ --scan-all --device cuda:0 --flash-attn --precision bf16
+REM cd /d "%~dp0"
+REM python abliterate.py -m E:\text-generation-webui\models\Nexesenex_Llama_3.2_3b_Costume_0.1 -o E:\text-generation-webui\models\Nexesenex_Llama_3.2_3b_Costume_0.1_abliterated_LPL --i E:\intermed\ --scan-all --device cuda:0 --flash-attn --precision bf16
+cd /d Q:\GitHub\abliteration
+python abliterate.py -m E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct -o E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct_abliterated --device cuda:0 --flash-attn --precision bf16
+cd /d Q:\GitHub\abliteration.nxs
+python abliterate.py -m E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct -o E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct_abliterated_LPL -i E:\intermed\ --scan-all --device cuda:0 --flash-attn --precision bf16
+pause
+pause
+cd /d Q:\GitHub\abliteration.nxs
+python abliterate.py -m E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct -o E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct_abliterated_LPL -i E:\intermed\ --scan-all --device cuda:0 --flash-attn --precision bf16
+pause
+pause
+cd /d Q:\GitHub\abliteration.nxs
+python abliterate.py -m E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct -o E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct_abliterated_LPL -i E:\intermed\ --scan-all --device cuda:0 --flash-attn --precision bf16
+pause
+pause
+cd /d Q:\GitHub\abliteration.nxs
+python abliterate.py -m E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct -o E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct_abliterated_LPL -i E:\intermed\ --scan-all --device cuda:0 --flash-attn --precision bf16
+pause
+pause
+cd /d Q:\GitHub\abliteration.nxs
+python abliterate.py -m E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct -o E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct_abliterated_LPL -i E:\intermed\ --scan-all --device cuda:0 --flash-attn --precision bf16
+pause
+pause
+cd /d Q:\GitHub\abliteration.nxs
+python abliterate.py -m E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct -o E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct_abliterated_LPL -i E:\intermed\ --scan-all --device cuda:0 --flash-attn --precision bf16
+pause
+pause
+cd /d Q:\GitHub\abliteration.nxs
+python abliterate.py -m E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct -o E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct_abliterated_LPL -i E:\intermed\ --scan-all --device cuda:0 --flash-attn --precision bf16
+pause
+pause
+cd /d Q:\GitHub\abliteration.nxs
+python abliterate.py -m E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct -o E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct_abliterated_LPL -i E:\intermed\ --scan-all --device cuda:0 --flash-attn --precision bf16
+pause
+pause
+cd /d Q:\GitHub\abliteration.nxs
+python abliterate.py -m E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct -o E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct_abliterated_LPL -i E:\intermed\ --scan-all -at --device cuda:0 --flash-attn --precision bf16
+pause
+pause
+cd /d Q:\GitHub\abliteration.nxs
+python abliterate.py -m E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct -o E:\text-generation-webui\models\Qwen_Qwen2.5-1.5B-Instruct_abliterated_LPL -i E:\intermed\ --scan-all -at --device cuda:0 --flash-attn --precision bf16
 pause
 pause
